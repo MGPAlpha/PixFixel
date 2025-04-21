de2197c6d2bff01c87f8251db32ecb05f144de56
Maeve Philipp <mgpalpha1@gmail.com>
2025-04-21 19:55:32 -0400

    add temp code for printing the average value of all pixels

--- PFDocument.gd
+++ PFDocument.gd
@@ -12,4 +12,12 @@ func _init(_path: String) -> void:
 	name = _path.get_file()
 	image = Image.new()
 	image.load(_path)
-	
+	#var avg_value = 0
+	#for i in image.get_width():
+		#for j in image.get_height():
+			#var color = image.get_pixel(i, j)
+			#avg_value += color.r
+			#avg_value += color.g
+			#avg_value += color.b
+	#avg_value /= (image.get_width() * image.get_height() * 3)
+	#print("Average value: ", avg_value)
